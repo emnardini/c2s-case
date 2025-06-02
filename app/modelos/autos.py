96bc48a8ce1a1db13f7100c9490d6aa74f901d65
emnardini <85845950+emnardini@users.noreply.github.com>
2025-06-02 10:19:06 -0300

    feat: criado o modelo de autos (reupload)

---
+++ app/modelos/autos.py
@@ -0,0 +1,26 @@
+# O uso de nullable=False visa aumentar a consistência do banco de dados, porém exige uma validação de dados mais rigorosa no preenchimento dos dados.
+# Esse aspecto é facilmente editável caso a fonte seja especialmente incosistente no fornecimento de algum dado específico
+
+# Composite indexing poderia aumentar ainda mais a performance, mas seu uso deve ser feito com cautela
+# por exemplo: na indexação composta de marca e modelo faz muito sentido, mas queries que não incluíssem a marca como "Quero um Corsa prata até 30 mil" podem ser prejudicadas.
+
+# A avaliação dos dois pontos comentados deve ser feita com base na utilização real
+
+from sqlalchemy import Column, Integer, String, Float, Boolean, Index
+from app.database.connection import Base
+
+
+class Autos(Base):
+    __tablename__ = "autos"
+
+    id = Column(Integer, primary_key=True)
+    marca = Column(String, nullable=False)
+    modelo = Column(String, nullable=False)
+    ano = Column(Integer, nullable=False)
+    cor = Column(String, nullable=False, index=True)
+    combustivel = Column(String, nullable=False, index=True)
+    kilometragem = Column(Float, nullable=False)
+    preco = Column(Float, nullable=False)
+    transmissao = Column(String, nullable=False, index=True)
+    portas = Column(Integer, nullable=False, index=True)
+    ar_condicionado = Column(Boolean, default=False, index=True)
