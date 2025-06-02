90d39d425fddd5e08aa140ee00a2848568b23258
emnardini <85845950+emnardini@users.noreply.github.com>
2025-06-02 18:16:56 -0300

    fix: pequenas correções nos inicializadores de pacotes

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,11 +1,10 @@
 import os
-from pathlib import Path
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-env_path = Path(__file__).resolve().parent.parent / ".env"
+env_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv(dotenv_path=env_path)
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
